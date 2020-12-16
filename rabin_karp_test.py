a0d0c71c8eb9742497985c0bc609089e0dc51f17
andrii.peleno <777andriypeleno777@gmail.com>
2020-12-16 17:39:16 +0200

    Add modulo division and spurious hit check

--- rabin_karp_test.py
+++ rabin_karp_test.py
@@ -9,6 +9,9 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(findAllMatches("Aabaab", "aab"), [3])
         self.assertEqual(findAllMatches("Kids are talking by the door", "talk"), [9])
         self.assertEqual(findAllMatches("aaaaa", "aa"), [0, 1, 2, 3])
+        self.assertEqual(len(findAllMatches("a" * 1000, "a")), 1000)
+        self.assertEqual(len(findAllMatches("a" * 1000, "aa")), 999)
+        self.assertEqual(len(findAllMatches("a" * 1000, "a"*100)), 901)
 
 
 
